2adb1e03d79c6c303d34cc41533b6f404faec675
YAGoOaR <yegorgribenko68@gmail.com>
2021-05-20 14:56:29 +0300

    Refactor threads start - part 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 
@@ -64,15 +65,17 @@ namespace lab8
                 {
                     FileWorker fw = new FileWorker(file);
 
-                    Thread thread1 = new Thread(new ParameterizedThreadStart(fw.write));
-                    thread1.Name = "1";
-                    thread1.Start("first line");
+                    const int threadsNumber = 2;
+                    List<Thread> threads = new List<Thread>();
 
-                    Thread thread2 = new Thread(new ParameterizedThreadStart(fw.write));
-                    thread2.Name = "2";
-                    thread2.Start("second line");
+                    for (int i = 1; i <= threadsNumber; i++) {
+                        Thread thread = new Thread(new ParameterizedThreadStart(fw.write));
+                        thread.Name = i.ToString();
+                        thread.Start($"line {i}");
+                        threads.Add(thread);
+                    }
 
-                    while (thread1.ThreadState != ThreadState.Stopped || thread2.ThreadState != ThreadState.Stopped) { }
+                    while (CheckThreads(threads)) { }
                     fw.Close();
                     file.Close();
                 }
@@ -87,15 +90,17 @@ namespace lab8
                 {
                     FileWorker fw = new FileWorker(file);
 
-                    Thread thread1 = new Thread(new ParameterizedThreadStart(fw.write_mutex));
-                    thread1.Name = "1";
-                    thread1.Start("first line");
+                    const int threadsNumber = 2;
+                    List<Thread> threads = new List<Thread>();
 
-                    Thread thread2 = new Thread(new ParameterizedThreadStart(fw.write_mutex));
-                    thread2.Name = "2";
-                    thread2.Start("second line");
+                    for (int i = 1; i <= threadsNumber; i++) {
+                        Thread thread = new Thread(new ParameterizedThreadStart(fw.write_mutex));
+                        thread.Name = i.ToString();
+                        thread.Start($"line {i}");
+                        threads.Add(thread);
+                    }
 
-                    while (thread1.ThreadState != ThreadState.Stopped || thread2.ThreadState != ThreadState.Stopped) { }
+                    while (CheckThreads(threads)) { }
                     fw.Close();
                     file.Close();
                 }
@@ -108,28 +113,17 @@ namespace lab8
                 {
                     FileWorker fw = new FileWorker(file);
 
-                    Thread thread1 = new Thread(new ParameterizedThreadStart(fw.write_sema));
-                    thread1.Name = "1";
-                    thread1.Start("first line");
+                    const int threadsNumber = 5;
+                    List<Thread> threads = new List<Thread>();
 
-                    Thread thread2 = new Thread(new ParameterizedThreadStart(fw.write_sema));
-                    thread2.Name = "2";
-                    thread2.Start("second line");
+                    for (int i = 1; i <= threadsNumber; i++) {
+                        Thread thread = new Thread(new ParameterizedThreadStart(fw.write_sema));
+                        thread.Name = i.ToString();
+                        thread.Start($"line {i}");
+                        threads.Add(thread);
+                    }
 
-                    Thread thread3 = new Thread(new ParameterizedThreadStart(fw.write_sema));
-                    thread3.Name = "3";
-                    thread3.Start("third line");
-
-                    Thread thread4 = new Thread(new ParameterizedThreadStart(fw.write_sema));
-                    thread4.Name = "4";
-                    thread4.Start("forth line");
-
-                    Thread thread5 = new Thread(new ParameterizedThreadStart(fw.write_sema));
-                    thread5.Name = "5";
-                    thread5.Start("fifth line");
-
-                    while (thread1.ThreadState != ThreadState.Stopped || thread2.ThreadState != ThreadState.Stopped
-                    || thread3.ThreadState != ThreadState.Stopped || thread4.ThreadState != ThreadState.Stopped || thread5.ThreadState != ThreadState.Stopped) { }
+                    while (CheckThreads(threads)) { }
                     fw.Close();
                     file.Close();
                 }
@@ -144,6 +138,12 @@ namespace lab8
             for (int i = 1; i <= 100000000; i++) {
                 sum += i;
             }
+        }
+
+        public static bool CheckThreads(List<Thread> threads) {
+            bool threadsRunning = false;
+            threads.ForEach((thread) => { threadsRunning = thread.ThreadState == ThreadState.Stopped ? threadsRunning : true; });
+            return threadsRunning;
         }
 
     }
